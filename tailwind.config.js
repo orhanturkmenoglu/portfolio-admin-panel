fdefc8efdd60f6a0b6532011cc95597ad3ab9ae5
orhan <orhantrkmn749@gmail.com>
2025-11-09 02:48:58 +0300

    chore: Install and configure TailwindCSS

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: '#ef4444',
+          500: '#ef4444',
+        },
+      },
+    },
+  },
+  plugins: [],
+}
+
